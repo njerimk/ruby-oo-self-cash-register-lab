8068c3d1eba10ec01b012f46d44023723e1f5736
njerimk <nmurugikamau@gmail.com>
2020-10-06 17:43:54 -0400

    Completed 7 tests

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,22 @@
+require 'pry'
 
+class CashRegister
+
+    attr_accessor :total, :discount
+
+    def initialize(discount=20)
+        @total = 0
+        @discount = discount
+    end
+
+    def add_item(title, price, quantity=1)
+        #  binding.pry
+        self.total += price * quantity
+        #why don't we need a title attribute accessor?
+    end
+    
+    def apply_discount 
+        
+    end 
+   
+end
